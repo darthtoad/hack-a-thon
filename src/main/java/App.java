83b4961b3052392ba356a374dadc16d94b50dcaa
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-01-12 15:32:03 -0800

    get all paths to work and eliminate errors

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,13 +17,6 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/teams/display", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            ArrayList teams = Team.getAllTeams();
-            model.put("teams", teams);
-            return new ModelAndView(model, "display.hbs");
-        }, new HandlebarsTemplateEngine());
-
         get("teams/new", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "new.hbs");
@@ -33,8 +26,8 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             String teamName = request.queryParams("team-name");
             String description = request.queryParams("description");
-            model.put("teamName", teamName);
-            model.put("description", description);
+            Team newTeam = new Team(teamName, description, new ArrayList<Member>());
+            model.put("newTeam", newTeam);
             return new ModelAndView(model, "members.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -50,7 +43,27 @@ public class App {
                 skills.add(skill);
             }
             Member newMember = new Member(firstName, lastName, email, favoriteColor, skills);
+            Team currentTeam = Team.findById(Team.getAllTeams().size());
+            ArrayList<Member> allMembers = currentTeam.getMembers();
+            allMembers.add(newMember);
+            Team.findById(Team.getAllTeams().size()).setMembers(allMembers);
+            model.put("allMembers", allMembers);
+            model.put("currentTeam", currentTeam);
             return new ModelAndView(model, "members.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/teams/display", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList teams = Team.getAllTeams();
+            model.put("teams", teams);
+            return new ModelAndView(model, "display.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/teams/display/finish", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList teams = Team.getAllTeams();
+            model.put("teams", teams);
+            return new ModelAndView(model, "display.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
