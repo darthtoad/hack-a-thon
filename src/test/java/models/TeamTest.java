1826a464aaff76f9b0f67e9fd6d5b512b58b0485
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-01-12 11:48:03 -0800

    make quick doublecheck test for returning second team correctly

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -139,6 +139,13 @@ public class TeamTest {
         assertEquals(1, Team.findById(t1.getTeamId()).getTeamId());
     }
 
+    @Test
+    public void findById_returnsCorrectSecondTeam_Team() throws Exception {
+        Team t1 = setupNewTeam();
+        Team t2 = setupNewTeam2();
+        assertEquals(2, Team.findById(t2.getTeamId()).getTeamId());
+    }
+
     public Member setupNewMember(){
         ArrayList newSkills = new ArrayList();
         newSkills.add("Throwing");
