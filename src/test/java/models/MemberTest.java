14f4f3fdc12c792171b19e7e6fec4b9bf0dda09f
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-01-19 11:42:25 -0800

    in MemberTest, readd and pass test for instantiating correctly

--- src/test/java/models/MemberTest.java
+++ src/test/java/models/MemberTest.java
@@ -9,23 +9,13 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class MemberTest {
-//    @Before
-//    public void setUp() throws Exception {
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        Member.clearAllMembers();
-//    }
-//
-//    @Test
-//    public void memberInstantiatesCorrectly() throws Exception {
-//        ArrayList skills = new ArrayList();
-//        skills.add("Java");
-//        Member testMember = new Member("Tim", "the Enchanter", "thing@thing.com", "blue", skills);
-//        assertEquals(true, testMember instanceof Member);
-//    }
-//
+
+    @Test
+    public void memberInstantiatesCorrectly() throws Exception {
+        Member testMember = new Member(1,"Tim", "the Enchanter", "thing@thing.com", "blue", "java");
+        assertEquals(true, testMember instanceof Member);
+    }
+
 //    @Test
 //    public void getFirstName_getsFirstName_Tim() throws Exception {
 //        ArrayList skills = new ArrayList();
