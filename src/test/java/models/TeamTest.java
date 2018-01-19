19763499a323df6878ea292ed74f20491d5ba1b8
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-01-19 11:26:57 -0800

    readd and pass second test in TeamTest.java

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -21,8 +21,8 @@ public class TeamTest {
 
     @Test
     public void TeamInstantiatesCorrectly() {
-        ArrayList skills = new ArrayList();
-        skills.add("Java");
+//        ArrayList skills = new ArrayList();
+//        skills.add("Java");
 //        Member testMember = new Member("Tim", "the Enchanter", "thing@thing.com", "blue", skills);
 //        ArrayList<Member> members = new ArrayList<>();
 //        members.add(testMember);
@@ -30,16 +30,11 @@ public class TeamTest {
         assertEquals(true, testTeam instanceof Team);
     }
 
-//    @Test
-//    public void getName_getsName_String() {
-//        ArrayList skills = new ArrayList();
-//        skills.add("Java");
-//        Member testMember = new Member("Tim", "the Enchanter", "thing@thing.com", "blue", skills);
-//        ArrayList<Member> members = new ArrayList<>();
-//        members.add(testMember);
-//        Team testTeam = new Team("The A-Team", "In 1972, a crack commando unit was sent to prison by a military court for a crime they didn't commit", members);
-//        assertEquals("The A-Team", testTeam.getName());
-//    }
+    @Test
+    public void getName_getsName_String() {
+        Team testTeam = new Team("The A-Team", "In 1972, a crack commando unit was sent to prison by a military court for a crime they didn't commit");
+        assertEquals("The A-Team", testTeam.getName());
+    }
 //
 //    @Test
 //    public void getDescription_getsDescription_String() {
